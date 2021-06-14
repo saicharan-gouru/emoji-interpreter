4ddf2a1ddbe3907ecc137f0db3c9a988560b7f4a
Saicharan Gouru <gourusaicharan6@gmail.com>
2021-06-14 12:02:33 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,54 @@
+import { useState } from "react";
+import "./styles.css";
+var emojiDict = {
+  "😊": "SMILING",
+  "😳": "DISBELIEF",
+  "😔": "SAD",
+  "🥡": "TAKEOUT BOX",
+  "❤️": "LOVE",
+  "😑": "ANNOYANCE",
+  "😀": "GRNNING FACE"
+};
+
+var emojisList = Object.keys(emojiDict);
+
+export default function App() {
+  var [meaning, setMeaning] = useState("");
+
+  function emojiInputHandler(event) {
+    var inputEmoji = event.target.value;
+    setMeaning(emojiDict[inputEmoji]);
+    if (emojiDict[inputEmoji] === undefined) {
+      setMeaning("Sorry..We don't have this in out database.. 😔");
+    }
+    if (inputEmoji === "") {
+      setMeaning("");
+    }
+  }
+
+  function emojiClickHandler(emoji) {
+    setMeaning(emojiDict[emoji]);
+  }
+
+  return (
+    <div className="App">
+      <h1>Emoji Interpreter</h1>
+      <input onChange={emojiInputHandler} />
+      <div class="meaning">
+        <h2>{meaning}</h2>
+      </div>
+      <h2>Emojis We Know</h2>
+      {emojisList.map((emoji) => {
+        return (
+          <span
+            class="emojisList"
+            key={emoji}
+            onClick={() => emojiClickHandler(emoji)}
+          >
+            {emoji}
+          </span>
+        );
+      })}
+    </div>
+  );
+}
